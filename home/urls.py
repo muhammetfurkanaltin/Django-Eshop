6a797b6c4e64359e6f45a3adb4b94bdbf6ab3a57
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2025-01-08 11:06:20 +0300

    mongodb eklendi

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
    path('',views.home),
    path('about',views.about),
    path('contact',views.contact),
+   path('test-mongodb/', views.test_mongodb_connection, name='test-mongodb'),
    
 ]
